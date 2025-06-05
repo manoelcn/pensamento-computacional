212f7be1bef5d55cb3b2907b49b1cb28cb7bda7f
manoel <candidomanoel78@gmail.com>
2025-06-05 11:00:31 -0300

    envia questao 4

---
+++ unidade3/Lista1/Questao4-Lista1.py
@@ -0,0 +1,11 @@
+x1 = float(input(""))
+y1 = float(input(""))
+x2 = float(input(""))
+y2 = float(input(""))
+px = float(input(""))
+py = float(input(""))
+
+if x1 <= px <= x2 and y1 <= py <= y2:
+    print("Dentro!")
+else:
+    print("Fora!") 
